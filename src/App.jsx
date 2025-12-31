4e04364078fe49becc2de5e88dee01efb38e83b2
MTerekar712 <mayurterekar712@gmail.com>
2025-12-31 22:44:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
-import navbar from './compnets/navbar'
-import home from './compnets/home'
-import Skill from './compnets/Skill'
-import contact from './compnets/contact'
-import {Route, Routes } from 'react-router-dom'
-import about from './compnets/about'
-import project from './compnets/project'
+// import navbar from './compnets/navbar'
+// import home from './compnets/home'
+// import Skill from './compnets/Skill'
+// import contact from './compnets/contact'
+// import {Route, Routes } from 'react-router-dom'
+// import about from './compnets/about'
+// import project from './compnets/project'
+
+// const App = () => {
+//   return (
+//     <div className='mainbox'>
+//       {navbar()}
+//       <Routes>
+//         <Route path='/' element={home()} />
+//         <Route path='/about' element={about()}/>
+//         <Route path='/skills' element={Skill()}/>
+//         <Route path='/project' element={project()} />
+//         <Route path='/contact' element={contact()}/>
+//       </Routes>
+//     </div>
+
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./compnets/Navbar";
+import Home from "./compnets/Home";
+import About from "./compnets/About";
+import Skill from "./compnets/Skill";
+import Project from "./compnets/Project";
+import Contact from "./compnets/Contact";
 
 const App = () => {
   return (
-    <div className='mainbox'>
-      {navbar()}
+    <BrowserRouter>
+      <Navbar />
+
       <Routes>
-        <Route path='/' element={home()} />
-        <Route path='/about' element={about()}/>
-        <Route path='/skills' element={Skill()}/>
-        <Route path='/project' element={project()} />
-        <Route path='/contact' element={contact()}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skill />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
