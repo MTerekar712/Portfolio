ff1f24aecf1b77f7be301016b24d48a38e598d63
MTerekar712 <mayurterekar712@gmail.com>
2026-01-01 07:14:23 +0530

    some

--- src/compnets/card.jsx
+++ src/compnets/card.jsx
@@ -1,48 +1,27 @@
-// import React from "react";
-// import "../pagesCss/card.css";
-// import photo1 from "../assets/PHOTO(2).webp";
-
-// const card = () => {
-//   return (
-//     <div className="card">
-//       <div className="photo1">
-//         <div className="photo" style={{backgroundImage:`url(${photo1})`}}></div>
-//       </div>
-//       <div className="info">
-//         {/* <h4>Hello!..</h4> */}
-//         <div className="mayu">
-//           <h1>I'm Mayur Terekar</h1>
-//           <h2>
-//             - Web Developer & <br />{" "}
-//             <h2 className="soft">SoftWare Developer</h2>
-//           </h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default card;
-
-
 import React from "react";
 import "../pagesCss/card.css";
-import photo1 from "../assets/PHOTO(2).webp";
+// import photo1 from "../assets/PHOTO(2).webp";
 
-const Card = () => {
+const card = () => {
   return (
     <div className="card">
-      <div className="photo" style={{ backgroundImage: `url(${photo1})` }}></div>
+      <div className="photo1">
+        <div className="photo" ></div>
+      </div>
       <div className="info">
+        {/* <h4>Hello!..</h4> */}
         <div className="mayu">
           <h1>I'm Mayur Terekar</h1>
-          <h2>- Web Developer &</h2>
-          <h2 className="soft">SoftWare Developer</h2>
+          <h2>
+            - Web Developer & <br />{" "}
+            <h2 className="soft">SoftWare Developer</h2>
+          </h2>
         </div>
       </div>
     </div>
   );
 };
 
-export default Card;
+export default card;
+
 
